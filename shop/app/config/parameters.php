1bb1d6284be0840378ca5ab7df07d79b72b4eb68
szymon002 <103931397+szymon002@users.noreply.github.com>
2023-11-17 13:38:39 +0100

    Initialize PrestaShop

---
+++ shop/app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'adidas-mariadb-server',
+    'database_port' => '',
+    'database_name' => 'adidas-mariadb',
+    'database_user' => 'root',
+    'database_password' => 'admin',
+    'database_prefix' => 'ad_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'BTeZRZqkJ0R0nqj1KPA462kD04VknJOgRITuRM6HntutKF2ssVmtjefPPR8OVZda',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-11-17',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'nv6gP3sfk7vCdf78Lm5J7HOcgK5LfYHk4z9FKDJh0IiysF4f2LjbhnUVUTLA59Kc',
+    'cookie_iv' => 'XoiNPgeoINzn4SHzdWYVP6CsDmPkHzRK',
+    'new_cookie_key' => 'def0000051ba1a133781ca3c5ac28a1844f1b956303b1add01cf7ca97d16a3c1d3644c948639b3f877dc43020350ece78e677a9fdb503393c74875e4c9a601b149c693ed',
+  ),
+);
